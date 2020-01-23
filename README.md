0ce6953ec41699b34f25572ccf2a26ba82f2923e
Piotr Zaniewski <piotr.zaniewski@fmc-ag.com>
2020-01-23 15:23:11 +0100

    added krew plugin cmd

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # kubectl with autompletion in a docker container
+How to build:
+docker build --rm -f "Dockerfile" -t piotrzan/kubectl-comp "."
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,16 @@
 mkdir /root/.kube
 
 echo "source <(kubectl completion bash)" >> ~/.bashrcource <(kubectl completion bash) # setup autocomplete in bash into the current shell, bash-completion package should be installed first. \
-source ~/.bashrc # add autocomplete permanently to your bash shell.
+
+(
+  set -x; cd "$(mktemp -d)" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/download/v0.3.3/krew.{tar.gz,yaml}" &&
+  tar zxvf krew.tar.gz &&
+  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
+  "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
+  "$KREW" update
+)
+
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+source ~/.bashrc

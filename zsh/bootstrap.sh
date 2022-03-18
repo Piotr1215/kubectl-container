7dcdd1ce21d6c3b7d636625d050ca71c30c60c76
Piotr Zaniewski <piotr@upbound.io>
2022-03-18 23:47:19 +0100

    K9s version bump

--- zsh/bootstrap.sh
+++ zsh/bootstrap.sh
@@ -7,7 +7,7 @@ mkdir /root/.kube
 echo '[STEP 2] Installing k9s awesomeness'
 (
   set -x &&
-  wget -c https://github.com/derailed/k9s/releases/download/v0.13.4/k9s_0.13.4_Linux_x86_64.tar.gz -O - | tar -xz &&
+  wget -c https://github.com/derailed/k9s/releases/download/v0.25.18/k9s_Linux_x86_64.tar.gz -O - | tar -xz &&
   chmod +x k9s &&
   mv k9s /usr/local/bin/
 )
